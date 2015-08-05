fc48c0c104db737b9fa00da9447e4cd38c6d6df8
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-08-05 00:48:43 -0700

    Add existing files for website

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>NuLEAF Technologies</title>
+
+      <link rel="shortcut icon" href="images/favicon.ico">
+
+      <link type="text/css" rel="stylesheet" href="css/main.css">
+      <link type="text/css" rel="stylesheet" href="css/bootstrap-responsive.min.css">
+      <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+
+      <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+      <noscript>Please enable JavaScript for website's full functionality.</noscript>
+   </head>
+   <body>
+      <div id="header">			
+         <div class="navbar">
+    	    <div class="navbar-inner">
+               <div class="nav-collapse container">
+                  <a class="brand" href="index.php">NuLEAF Technologies</a>
+    		  <ul class="nav">
+                     <li><a href="forum/">Forum</a></li>
+    		  </ul>
+    	       </div>
+   	    </div>
+	 </div>	
+      </div>
+      <div class="content-container" id="index-content">
+
+         <center><a href="index.php"><img src="images/banner.jpg" /></a></center><br>
+
+         <!-- Left column -->
+         <div style="float:left; width:45%;">
+         <p><font size="4" color="#95B959">LATEST UPDATES</font></p><hr>
+         <table class="table table-striped">
+            <tr>
+               <td><b>August 7, 2015 - Teaching Meeting 3:30pm-6pm</b><br>Meeting Room at Saratoga Library<br><br>What is the Scientific Method and How To Design an Experiment with Dr. Wenonah Vercoutere</td>
+            </tr>
+            <tr>
+               <td><b>July 31, 2015 - Teaching Meeting 3pm-5pm</b><br>Meeting Room at Saratoga Library<br><br>Protein Folding and Physical Biomodeling with Dr. Promita Chakraborty</td>
+            </tr>
+            <tr>
+               <td><b>July 10, 2015 - Summer Social 1pm-3pm</b><br>Varian Park, Cupertino</td>
+            </tr>   
+            <tr>
+               <td><b>July 5, 2015 - Summer applications close at midnight.</b></td>
+            </tr>      
+         </table>
+         </div>
+
+         <!-- Right column -->
+         <div style="float:right; width:50%; ">
+            <p><font size="4" color="#95B959">FEATURED PROJECTS</font></p><hr>
+            <p><font size="4" color="#95B959">TEACHING MEETINGS</font></p><hr>
+            <center><img src="http://www.quezylab.com/home/peppytideModel.png"/></center>
+            <font size="4" color="black"><b>Protein Folding and Physical Biomodeling - 7/31/2015</b></font></p>
+            <div>Dr. Promita Chakraborty is well versed in protein folding and is exploring the new frontier of physical biomodeling.
+               Physical biomodeling is a novel endeavor that intersects natural science, computational science, and precision physical modeling.
+               She founded QuezyLab, which seeks to connect the macro-world to the nano-world. 
+               <br><b>For more information:</b><br>
+               QuezyLab, <a href="http://www.quezylab.com/">http://www.quezylab.com/</a><br>
+               Peppytide & Peppy Molecules, <a href="http://www.peppytides.org/">http://www.peppytides.org/</a><br>
+               Homepage: <a href="http://people.cs.vt.edu/~promita9/">http://people.cs.vt.edu/~promita9/</a><br>
+            </div>
+         </div>
+
+      </div>
+   </body>
+</html>
