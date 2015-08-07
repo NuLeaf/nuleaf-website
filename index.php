1f9601916f2e1e275a72cff793dc9e9fe52b7ddb
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-08-07 13:51:55 -0700

    Merge Nova's placeholder changes
    -Update index.php
    -Update css/main.css
    -Add to images/

--- index.php
+++ index.php
@@ -33,36 +33,60 @@
 
          <!-- Left column -->
          <div style="float:left; width:45%;">
-         <p><font size="4" color="#95B959">LATEST UPDATES</font></p><hr>
-         <table class="table table-striped">
-            <tr>
-               <td><b>August 7, 2015 - Teaching Meeting 3:30pm-6pm</b><br>Meeting Room at Saratoga Library<br><br>What is the Scientific Method and How To Design an Experiment with Dr. Wenonah Vercoutere</td>
-            </tr>
-            <tr>
-               <td><b>July 31, 2015 - Teaching Meeting 3pm-5pm</b><br>Meeting Room at Saratoga Library<br><br>Protein Folding and Physical Biomodeling with Dr. Promita Chakraborty</td>
-            </tr>
-            <tr>
-               <td><b>July 10, 2015 - Summer Social 1pm-3pm</b><br>Varian Park, Cupertino</td>
-            </tr>   
-            <tr>
-               <td><b>July 5, 2015 - Summer applications close at midnight.</b></td>
-            </tr>      
-         </table>
+            <p><font size="4" color="#95B959">LATEST UPDATES</font></p><hr>
+            <table class="table table-striped">
+               <tr>
+                  <td><b>August 7, 2015 - Teaching Meeting 3:30pm-5:30pm</b><br>Meeting Room at Saratoga Library<br><br>Introduction to Nanotechnology with Margaret Ackroyd</td>
+               <tr>
+                  <td><b>July 31, 2015 - Teaching Meeting 3pm-5pm</b><br>Meeting Room at Saratoga Library<br><br>Protein Folding and Physical Biomodeling with Dr. Promita Chakraborty</td>
+               </tr>
+               <tr>
+                  <td><b>July 10, 2015 - Summer Social 1pm-3pm</b><br>Varian Park, Cupertino</td>
+               </tr>   
+               <tr>
+                  <td><b>July 5, 2015 - Summer applications close.</b></td>
+               </tr>      
+            </table>
+            <p><font size="4" color="#95B959">CONTACT US</font></p><hr>
+            <div>Interested in getting involved, want more information?<br>Email to: <a href="mailto:nuleaftechnologies@gmail.com">nuleaftechnologies@gmail.com</a></div>
          </div>
 
          <!-- Right column -->
          <div style="float:right; width:50%; ">
-            <p><font size="4" color="#95B959">FEATURED PROJECTS</font></p><hr>
+            <p><font size="4" color="#95B959">OVERVIEW</font></p><hr>
+            <div>
+               Producing electricity from plants is a powerful, established concept evident in up-and-coming research. Past methods have involved the use of nanotechnology or natural soil chemistry.
+               These studies demonstrate both feasibility and ability to commercialize.  However, previous investigations are limited by their low power generation, 
+               interference with natural plant processes, and reliance on the inefficiency of the photosynthetic pathway.<br><br>
+               In contrast, this project proposes a technique that could harness higher levels of power with minimal ecological impact.  The process will not rely on photosynthesis 
+               and will be harmlessly integrated into the plant’s biological system and surrounding environment.  This research hinges upon nanogenetics and the use of the plant’s own carbon storage 
+               or other biological pathways to create the nanoparticles of the coating and bioelectric network.  In comparison to solar panels, this project has the potential to be cheaper, less toxic, 
+               and more efficient by using natural materials and not requiring constant maintenance.<br><br>
+            </div>
             <p><font size="4" color="#95B959">TEACHING MEETINGS</font></p><hr>
-            <center><img src="http://www.quezylab.com/home/peppytideModel.png"/></center>
-            <font size="4" color="black"><b>Protein Folding and Physical Biomodeling - 7/31/2015</b></font></p>
-            <div>Dr. Promita Chakraborty is well versed in protein folding and is exploring the new frontier of physical biomodeling.
-               Physical biomodeling is a novel endeavor that intersects natural science, computational science, and precision physical modeling.
-               She founded QuezyLab, which seeks to connect the macro-world to the nano-world. 
-               <br><b>For more information:</b><br>
-               QuezyLab, <a href="http://www.quezylab.com/">http://www.quezylab.com/</a><br>
-               Peppytide & Peppy Molecules, <a href="http://www.peppytides.org/">http://www.peppytides.org/</a><br>
-               Homepage: <a href="http://people.cs.vt.edu/~promita9/">http://people.cs.vt.edu/~promita9/</a><br>
+            <div class="teaching-meeting">
+               <p><font size="4" color="black"><b>Introduction to Nanotechnology - 8/7/2015</b></font></p>
+               <img class="image-left" src="images/MargaretAckroyd.jpg" width="150px"/>
+               <div class="text-wrap-right">
+                  Margaret Ackroyd is a bioengineering major working towards a career in bio-nanotechnology research. <br>
+                  Bionanotechnology is a rapidly growing field that denotes research efforts tasked with exploring the relationships between nanoscience and biology;
+                  one such aspect would be biomimetic design on the nanoscale.<br><br>
+                  Margaret Ackroyd is currently a student at Foothill College. Margaret has dabbled in many areas of expertise, 
+                  some of which include design, medicine, business law, computer science, environmental stewardship, and entrepreneurship.
+               </div>
+            </div>
+            <div class="teaching-meeting">
+               <p><font size="4" color="black"><b>Protein Folding and Physical Biomodeling - 7/31/2015</b></font></p>
+               <img class="image-left" src="images/PromitaChakraborty.jpg"/>
+               <div class="text-wrap-right">
+                  Dr. Promita Chakraborty is well versed in protein folding and is the exploring the new frontier of physical biomodeling.
+                  Physical biomodeling is a novel endeavor that intersects natural science, computational science, and precision physical modeling.
+                  She founded QuezyLab, which seeks to connect the macro-world to the nano-world. 
+                  <br><b>For more information:</b><br>
+                  QuezyLab, <a href="http://www.quezylab.com/">http://www.quezylab.com/</a><br>
+                  Peppytide & Peppy Molecules, <a href="http://www.peppytides.org/">http://www.peppytides.org/</a><br>
+                  Homepage: <a href="http://people.cs.vt.edu/~promita9/">http://people.cs.vt.edu/~promita9/</a>
+               </div>
             </div>
          </div>
 
