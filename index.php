54d7857d05514fbce25ac0c0808b471d7dea07a6
Nova Ng <novang@gmail.com>
2015-08-08 18:34:53 -0700

    Merge with Nova's placeholder changes

--- index.php
+++ index.php
@@ -1,95 +1,92 @@
 ﻿<html>
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <title>NuLEAF Technologies</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>NuLEAF Technologies</title>
+  
+        <link rel="shortcut icon" href="images/favicon.ico">
 
-      <link rel="shortcut icon" href="images/favicon.ico">
+        <link type="text/css" rel="stylesheet" href="css/main.css?v=1.1">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
 
-      <link type="text/css" rel="stylesheet" href="css/main.css?v=1.1">
-      <link type="text/css" rel="stylesheet" href="css/bootstrap-responsive.min.css">
-      <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-      <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-      <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <noscript>Please enable JavaScript for website's full functionality.</noscript>
+    </head>
+    <body>
+        <div id="header">			
+            <div class="navbar">
+    	        <div class="navbar-inner">
+                    <div class="nav-collapse container">
+                        <a class="brand" href="index.php">NuLEAF Technologies</a>
+    		        <ul class="nav">
+                            <li><a href="forum/">Forum</a></li>
+    		        </ul>
+    	            </div>
+   	        </div>
+	    </div>	
+        </div>
 
-      <noscript>Please enable JavaScript for website's full functionality.</noscript>
-   </head>
-   <body>
-      <div id="header">			
-         <div class="navbar">
-    	    <div class="navbar-inner">
-               <div class="nav-collapse container">
-                  <a class="brand" href="index.php">NuLEAF Technologies</a>
-    		  <ul class="nav">
-                     <li><a href="forum/">Forum</a></li>
-    		  </ul>
-    	       </div>
-   	    </div>
-	 </div>	
-      </div>
-      <div class="content-container" id="index-content">
-
-         <center><a href="index.php"><img src="images/banner.jpg" /></a></center><br>
-
-         <!-- Left column -->
-         <div style="float:left; width:45%;">
-            <p><font size="4" color="#95B959">LATEST UPDATES</font></p><hr>
-            <table class="table table-striped">
-               <tr>
-                  <td><b>August 7, 2015 - Teaching Meeting 3:30pm-5:30pm</b><br>Meeting Room at Saratoga Library<br><br>Introduction to Nanotechnology with Margaret Ackroyd</td>
-               <tr>
-                  <td><b>July 31, 2015 - Teaching Meeting 3pm-5pm</b><br>Meeting Room at Saratoga Library<br><br>Protein Folding and Physical Biomodeling with Dr. Promita Chakraborty</td>
-               </tr>
-               <tr>
-                  <td><b>July 10, 2015 - Summer Social 1pm-3pm</b><br>Varian Park, Cupertino</td>
-               </tr>   
-               <tr>
-                  <td><b>July 5, 2015 - Summer applications close.</b></td>
-               </tr>      
-            </table>
-            <p><font size="4" color="#95B959">CONTACT US</font></p><hr>
-            <div>Interested in getting involved, want more information?<br>Email to: <a href="mailto:nuleaftechnologies@gmail.com">nuleaftechnologies@gmail.com</a></div>
-         </div>
-
-         <!-- Right column -->
-         <div style="float:right; width:50%; ">
-            <p><font size="4" color="#95B959">OVERVIEW</font></p><hr>
-            <div>
-               Producing electricity from plants is a powerful, established concept evident in up-and-coming research. Past methods have involved the use of nanotechnology or natural soil chemistry.
-               These studies demonstrate both feasibility and ability to commercialize.  However, previous investigations are limited by their low power generation, 
-               interference with natural plant processes, and reliance on the inefficiency of the photosynthetic pathway.<br><br>
-               In contrast, this project proposes a technique that could harness higher levels of power with minimal ecological impact.  The process will not rely on photosynthesis 
-               and will be harmlessly integrated into the plant’s biological system and surrounding environment.  This research hinges upon nanogenetics and the use of the plant’s own carbon storage 
-               or other biological pathways to create the nanoparticles of the coating and bioelectric network.  In comparison to solar panels, this project has the potential to be cheaper, less toxic, 
-               and more efficient by using natural materials and not requiring constant maintenance.<br><br>
+        <div class="content-container" id="index-content">
+            <a href="index.php"><img id="banner" src="images/banner.jpg" /></a><br>
+ 
+            <div id="left-column">
+                <p class="section-title">LATEST UPDATES</p><hr>
+                <table class="table table-striped">
+                    <tr>
+                        <td><span class="title">August 7, 2015 - Teaching Meeting 3:30pm-5:30pm</span><br>Meeting Room at Saratoga Library<br><br>Introduction to Nanotechnology with Margaret Ackroyd</td>
+                    <tr>
+                        <td><span class="title">July 31, 2015 - Teaching Meeting 3pm-5pm</span><br>Meeting Room at Saratoga Library<br><br>Protein Folding and Physical Biomodeling with Dr. Promita Chakraborty</td>
+                    </tr>
+                    <tr>
+                        <td><span class="title">July 10, 2015 - Summer Social 1pm-3pm</span><br>Varian Park, Cupertino</td>
+                    </tr>   
+                    <tr>
+                        <td><span class="title">July 5, 2015 - Summer applications close.</span></td>
+                    </tr>      
+                </table>
+                <p class="section-title">CONTACT US</p><hr>
+                <div>Interested in getting involved, want more information?<br>Email to: <a href="mailto:nuleaftechnologies@gmail.com">nuleaftechnologies@gmail.com</a></div>
             </div>
-            <p><font size="4" color="#95B959">TEACHING MEETINGS</font></p><hr>
-            <div class="teaching-meeting">
-               <p><font size="4" color="black"><b>Introduction to Nanotechnology - 8/7/2015</b></font></p>
-               <img class="image-left" src="images/MargaretAckroyd.jpg" width="150px"/>
-               <div class="text-wrap-right">
-                  Margaret Ackroyd is a bioengineering major working towards a career in bio-nanotechnology research. <br>
-                  Bionanotechnology is a rapidly growing field that denotes research efforts tasked with exploring the relationships between nanoscience and biology;
-                  one such aspect would be biomimetic design on the nanoscale.<br><br>
-                  Margaret Ackroyd is currently a student at Foothill College. Margaret has dabbled in many areas of expertise, 
-                  some of which include design, medicine, business law, computer science, environmental stewardship, and entrepreneurship.
-               </div>
-            </div>
-            <div class="teaching-meeting">
-               <p><font size="4" color="black"><b>Protein Folding and Physical Biomodeling - 7/31/2015</b></font></p>
-               <img class="image-left" src="images/PromitaChakraborty.jpg"/>
-               <div class="text-wrap-right">
-                  Dr. Promita Chakraborty is well versed in protein folding and is the exploring the new frontier of physical biomodeling.
-                  Physical biomodeling is a novel endeavor that intersects natural science, computational science, and precision physical modeling.
-                  She founded QuezyLab, which seeks to connect the macro-world to the nano-world. 
-                  <br><b>For more information:</b><br>
-                  QuezyLab, <a href="http://www.quezylab.com/">http://www.quezylab.com/</a><br>
-                  Peppytide & Peppy Molecules, <a href="http://www.peppytides.org/">http://www.peppytides.org/</a><br>
-                  Homepage: <a href="http://people.cs.vt.edu/~promita9/">http://people.cs.vt.edu/~promita9/</a>
-               </div>
-            </div>
-         </div>
 
-      </div>
-   </body>
+            <div id="right-column">
+                <p class="section-title">OVERVIEW</p><hr>
+                <div>
+                    Producing electricity from plants is a powerful, established concept evident in up-and-coming research. Past methods have involved the use of nanotechnology or natural soil chemistry.
+                    These studies demonstrate both feasibility and ability to commercialize.  However, previous investigations are limited by their low power generation, 
+                    interference with natural plant processes, and reliance on the inefficiency of the photosynthetic pathway.<br><br>
+                    In contrast, this project proposes a technique that could harness higher levels of power with minimal ecological impact.  The process will not rely on photosynthesis 
+                    and will be harmlessly integrated into the plant’s biological system and surrounding environment.  This research hinges upon nanogenetics and the use of the plant’s own carbon storage 
+                    or other biological pathways to create the nanoparticles of the coating and bioelectric network.  In comparison to solar panels, this project has the potential to be cheaper, less toxic, 
+                    and more efficient by using natural materials and not requiring constant maintenance.<br><br>
+                </div>
+                <p class="section-title">TEACHING MEETINGS</p><hr>
+                <div class="teaching-meeting">
+                    <p class="title">Introduction to Nanotechnology - 8/7/2015</p>
+                    <img class="image-left" src="images/MargaretAckroyd.jpg"/>
+                    <div class="text-wrap-right">
+                        Margaret Ackroyd is a bioengineering major working towards a career in bio-nanotechnology research. <br>
+                        Bionanotechnology is a rapidly growing field that denotes research efforts tasked with exploring the relationships between nanoscience and biology;
+                        one such aspect would be biomimetic design on the nanoscale.<br><br>
+                        Margaret Ackroyd is currently a student at Foothill College. Margaret has dabbled in many areas of expertise, 
+                        some of which include design, medicine, business law, computer science, environmental stewardship, and entrepreneurship.
+                    </div>
+                </div>
+                <div class="teaching-meeting">
+                    <p class="title">Protein Folding and Physical Biomodeling - 7/31/2015</p>
+                    <img class="image-left" src="images/PromitaChakraborty.jpg"/>
+                    <div class="text-wrap-right">
+                        Dr. Promita Chakraborty is well versed in protein folding and is the exploring the new frontier of physical biomodeling.
+                        Physical biomodeling is a novel endeavor that intersects natural science, computational science, and precision physical modeling.
+                        She founded QuezyLab, which seeks to connect the macro-world to the nano-world. 
+                        <br><b>For more information:</b><br>
+                        QuezyLab, <a href="http://www.quezylab.com/">http://www.quezylab.com/</a><br>
+                        Peppytide & Peppy Molecules, <a href="http://www.peppytides.org/">http://www.peppytides.org/</a><br>
+                        Homepage: <a href="http://people.cs.vt.edu/~promita9/">http://people.cs.vt.edu/~promita9/</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
