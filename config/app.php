6dce032b159943785d3c1ca46e4414be8d2db26d
NuLeaf Technologies <nuleaf@nuleaftech.com>
2016-06-10 03:01:23 -0400

    Moved to production.

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost/nuleaf/website',
+    'url' => 'https://nuleaftech',
 
     /*
     |--------------------------------------------------------------------------
